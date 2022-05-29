ddae398a1394088c41df32ef84fddb0bd6be2409
elhajuojy <souirimehdi311@gmail.com>
2022-05-29 23:40:16 +0100

    category_item

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    Key? key,
+    required this.title,
+    required this.color,
+    required this.id,
+  }) : super(key: key);
+
+  final String id;
+  final String title;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+              colors: [color, color.withOpacity(0.7)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
+        padding: EdgeInsets.all(25),
+        child: Text(
+          title,
+          style: TextStyle(
+              fontSize: 20, fontFamily: 'Ubuntu', fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
