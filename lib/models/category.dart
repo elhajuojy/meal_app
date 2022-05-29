594b89d2e3940e8bea20f4e186f2372f763664e2
elhajuojy <souirimehdi311@gmail.com>
2022-05-29 14:17:14 +0100

    add models

---
+++ lib/models/category.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Catergory {
+  final String id;
+  final String title;
+  final Color color;
+
+  const Catergory({required this.id, required this.title, required this.color});
+}
